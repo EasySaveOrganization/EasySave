564e0d27ce47313aceb9e4660b02b256edef23f0
Malek Nasri <malek.nasri@viacesi.fr>
2024-02-08 12:25:37 +0100

    liaison :)

--- EasySaveProjectCode/EasySaveProject/obj/Debug/net8.0/EasySaveProject.AssemblyInfo.cs
+++ EasySaveProjectCode/EasySaveProject/obj/Debug/net8.0/EasySaveProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySaveProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+564ffa77b380d071e4c635bccc10f744e53f978e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c461d8e013768bcc0fbe9b74aea7b83029cbd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySaveProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySaveProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
