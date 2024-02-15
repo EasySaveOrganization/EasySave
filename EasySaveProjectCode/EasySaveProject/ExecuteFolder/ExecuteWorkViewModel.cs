ea9482e43ae93916d25be11706609b02d8f2a22e
Shosan <valentin.girod@icloud.com>
2024-02-15 10:09:33 +0100

    changing the choosing method for executing work to an int

--- EasySaveProjectCode/EasySaveProject/ExecuteFolder/ExecuteWorkViewModel.cs
+++ EasySaveProjectCode/EasySaveProject/ExecuteFolder/ExecuteWorkViewModel.cs
@@ -17,20 +17,21 @@ namespace EasySaveProject.ExecuteFolder
         public void chooseSaveWork()
         {
             var workList = workListService.LoadWorkListFromFile();
-            int i = 0;
+            int i = 1;
             foreach (var work in workList)
             {
                 Console.WriteLine(i + " - Name of the save work : " + work.saveName + "\n");
                 i += 1;
             }
-            string? inputSavework = Console.ReadLine();
-            foreach (var work in workList)
+            int selectedIndex;
+            while (!int.TryParse(Console.ReadLine(), out selectedIndex) || selectedIndex < 1 || selectedIndex > workList.Count)
             {
-                if (work.saveName == inputSavework)
-                {
-                    executeWorkService.executeWork(work);
-                }
+                Console.WriteLine("Veuillez entrer un numéro valide.");
+                Console.Write("Veuillez entrer le numéro du travail que vous souhaitez exécuter : ");
             }
+            // Récupération du travail sélectionné
+            SaveWorkModel selectedWork = workList[selectedIndex - 1];
+            executeWorkService.executeWork(selectedWork);
 
         }
     }
--- EasySaveProjectCode/Unit test/obj/Debug/net8.0/Unit test.AssemblyInfo.cs
+++ EasySaveProjectCode/Unit test/obj/Debug/net8.0/Unit test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unit test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5de31d12b28134f4a948dc1b69fdf4497b8e79c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e121593bbb3814395385a9f122a2cf1d83e43fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unit test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unit test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
