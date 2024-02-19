d7d092765aaaded1d1c585c7c4e193494852e95a
Shosan <valentin.girod@icloud.com>
2024-02-19 16:40:20 +0100

    changed the langagemanager for the menu

--- EasySaveProjectCode/Unit test/obj/Debug/net8.0/Unit test.AssemblyInfo.cs
+++ EasySaveProjectCode/Unit test/obj/Debug/net8.0/Unit test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unit test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aacd238ba3621cf33ed5c255d1ed70563641e462")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc07980a658bff3917e1b58d5abd8e1da3c6386")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unit test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unit test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
