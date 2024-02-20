fbd8924b753897c92fbf6e90dc235c1dfe31f255
Malekkkkk <100804213+Malekkkkk@users.noreply.github.com>
2024-02-20 17:37:06 +0100

    Update MenuRouteur.cs

--- EasySaveProjectCode/EasySaveProject/MenuFolder/MenuRouteur.cs
+++ EasySaveProjectCode/EasySaveProject/MenuFolder/MenuRouteur.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EasySaveProject.SateFolder;
 using static EasySaveProject.MenuFolder.MenuViewModel;
 
 namespace EasySaveProject.MenuFolder
