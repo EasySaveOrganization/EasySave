5fa7fe2bcb60350d78ba8a640413c811069771d6
Malekkkkk <100804213+Malekkkkk@users.noreply.github.com>
2024-02-20 17:32:36 +0100

    Update SaveWorkViewModel.cs

--- EasySaveProjectCode/EasySaveProject/SaveWorkFolder/SaveWorkViewModel.cs
+++ EasySaveProjectCode/EasySaveProject/SaveWorkFolder/SaveWorkViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EasySaveProject.SaveWorkFolder;
+using EasySaveProject.ExecuteFolder;
 
 namespace EasySaveProject.SaveWorkFolder
 {
